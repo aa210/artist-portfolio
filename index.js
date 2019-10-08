115a7d6c9c8afdd9f7dec30fc6ac72d5efed6752
Adeola Adesida <aadesida11@gmail.com>
2019-10-08 02:01:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 $(document).ready(function() { 
+   //Scroll Up Menu
+var didScroll;
+        var lastScrollTop = 0;
+        var delta = 5;
+        var navbarHeight = $(".navbar, #topDivMobile").outerHeight();
+         $(document).scroll(function () {
+            var y = $(document).scrollTop(),
+                header = $(".navbar, #topDivMobile");
+        
+            if (y >= 160) {
+                var st = $(this).scrollTop();
  
+            // Make sure they scroll more than delta
+            if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+                                                
+            // If they scrolled down and are past the navbar, add class .nav-up.
+            // This is necessary so you never see what is "behind" the navbar.
+            if (st > lastScrollTop && st > navbarHeight){
+                // Scroll Down                
+                header.removeClass('fixed');
+            } else {
+                // Scroll Up                
+                if(st + $(window).height() < $(document).height() && y > 142) {
+                    header.addClass('fixed');                    
+                }else if(y <= 142){
+                    header.removeClass('fixed');
+                }
+            }
+ 
+            lastScrollTop = st; 
+            } else {
+                header.removeClass('fixed');
+            }
+        });
+
+  
+});
+
+
+
+//Mobile Menu
+ $("#mobileMenuButton").click(function(){
+    $("#navigationMobile").slideToggle();
+  }); 
+  
+ $("#viewMenu").click(function(){
+    $("#menuPrint").slideToggle();
+ }); 
+   
+   
+ //PORTFOLIO 
 $(".websites").show()
 $("#webLink").addClass("selected");  
 $(".webapps").hide();
@@ -18,11 +69,8 @@ $(".webapps").hide();
     $(".websites").hide(500);
   });
   
-  
-  
-});
-
-
+ 
+ 
 /*
 https://postimg.cc/gallery/msykkeqi/9b1ecf1d/
 
