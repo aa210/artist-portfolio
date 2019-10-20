5c37e07981a8e75b8263db56c3eb42be59e6b9b3
Adeola Adesida <aadesida11@gmail.com>
2019-10-20 19:44:51 -0400

    Update script.js

--- SalonSpa/script.js
+++ SalonSpa/script.js
@@ -52,8 +52,8 @@ $(function() {
  }); 
   
 //Gallery Click
-$("#hair").show();
-$("#spaStudio").hide();
+$("#hair").hide();
+$("#spaStudio").show();
 $("#nails").hide();
 $("#beauty").hide(); 
   
@@ -160,3 +160,102 @@ $("#left1, #right1").show(); $("#right3, #right2, #left3, #left2").hide();
  
    
 });
+
+/*
+
+ /*
+  https://i.pinimg.com/736x/0d/53/b4/0d53b4dbdcfef6d7a37bafeae2e3ebba.jpg
+ 
+ https://i.pinimg.com/236x/46/b6/1a/46b61a31f7e1d8c19b8ccd519078068a.jpg
+ 
+ https://img.allw.mn/content/makeup/2011/09/bride-getting-ready_make-up-black-white_Real-Wedding_CrystalBen_Perez-Photography_southern-weddings-dec09.jpg
+ 
+
+
+2. Services Menu Page
+- service, amount of time, price menu list
+https://www.hairworkzkapiti.co.nz/pricing.html
+https://hair-beauty.vamtam.com/contact-us/
+https://hair-beauty.vamtam.com/pricing/
+https://salon.freevision.me/services/
+https://www.placid.in/services/skin
+https://www.nafsalon.com/prices
+https://curly.mikado-themes.com/
+—
+https://relaxandrenewmassage.org/policy
+https://www.rubyroom.com/pages/massage
+
+Write Full Menu w/ 3 Columns, Divided By Section”
+1.HAIR
+Prices  from
+*Cuts
+- Woman cut
+- Man cut
+- Child Boy Cut
+- Child Girl Cut
+*Styles:
+- Box Braids ……. $60+
+- Crochet Braids……$60+
+- Faux Locs…….$100+
+- Senegalese Twists
+- Sew In w/ Closure
+- Sew In w/ Leave Out
+- Wig Install
+- Ponytails
+- Perm
+- Keratin
+- Roller Set
+- Blow Dry
+
+*Color:
+- Tint (regrowth)
+- Tint(long hair)
+- Foil Highlights
+- Half Head
+- Full Head
+- Ombre
+2.SKIN+Nails (page/section)
+*Nails
+- Facials (Header): types list+time+price
+- Threading
+- Waxing
+- Treatments
+3.SPA:
+- Massage(types)
+- Foot Therapy
+- Body Scrub
+4.PACKAGES:
+Text Info:
+- https://www.placid.in/services
+- https://lapeersalon.com/
+- https://www.thehairdon.com/
+- https://www.hairworkzkapiti.co.nz/pricing.html
+- http://fuschiatherapy.com/services/
+- https://blowoutjunkie.com/services-menu/bridal/
+- http://www.jemome.com/cdn/2013/07/nail-salon-service-menu_280749.jpg
+- http://cdn.editthewebsite.com/pictures/full/e068789f.jpg
+- http://polishedroc.com/files/2017/08/updated-1-1.jpg?w=1060&h=795&a=t
+3. Booking Calendar Page
+https://salon.freevision.me/booking/
+https://pur.vamtam.com/appointments
+4. Treatment Descriptions?
+
+jQuery:
+- https://www.w3schools.com/jquery/jquery_ref_effects.asp
+
+Testimonials:
+- https://curly.mikado-themes.com/elements/testimonials/
+- https://hair-beauty.vamtam.com/reviews/
+- https://www.hairbyestylez.com/
+- http://preview.themeforest.net/item/hair-salon-wordpress-theme-hair-salon-wp/full_screen_preview/17739078?_ga=2.143291829.1613731331.1564806537-1640327584.1564800416
+
+Developer Tools:
+- www.capterra.com/appointment-scheduling-software/
+- schedulicity.com
+6. Treatment/Services Descriptions
+https://pur.vamtam.com/treatments/salt-therapies/
+—
+
+
+
+*/
