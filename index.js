fc900c41b7fe3130602dcbbf8b03691fa52cb928
Adeola Adesida <aadesida11@gmail.com>
2024-09-23 17:03:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,13 @@ $("#logo").ready(function() {
         });
     
   $(function() {
-  $("#artWork").mouseover(function() {
-    $(".subLinks").addClass("subLinks-change");
+  $("#art-work").mouseover(function() {
+    $(".sub-links").addClass("sub-links-change");
   });
-  $(".subLinks").mouseleave(function() {
-    $(".subLinks").removeClass("subLinks-change");
+  $(".sub-links").mouseleave(function() {
+    $(".sub-links").removeClass("sub-links-change");
   });
-    
-
   
-});
-  
-  
-
+});  
    
   });
